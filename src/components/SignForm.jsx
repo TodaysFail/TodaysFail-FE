344291e961409172069a94f77f9bc2b479956d46
ssaid <94416699+swywssaid@users.noreply.github.com>
2023-02-25 18:23:37 +0900

    [#2] feat: 닉네임 입력창 구현
    - 유효성 검사 (임시: 영문 소문자와 숫자로 최대 12자까지 가능)
    - 유효성 검사 통과 시, 시작하기 버튼 활성화

---
+++ src/components/SignForm.jsx
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+
+export default function SignForm() {
+  const [nickname, setNickname] = useState('');
+  const [isValid, setIsValid] = useState(false);
+  const [warningMessage, setWarningMessage] = useState('');
+
+  const checkValidNickname = (event) => {
+    const input = event.target.value;
+    const regex = /^[a-z0-9]{1,12}$/; // 영문 소문자와 숫자만 사용 가능하고 최소 길이는 1 최대 길이는 12
+
+    if (regex.test(input)) {
+      setNickname(input);
+      setIsValid(true);
+      setWarningMessage('');
+    } else {
+      setIsValid(false);
+      setWarningMessage('영문 소문자와 숫자 조합으로 최대 12자까지 가능합니다');
+    }
+
+    if (input === '') {
+      setNickname('');
+      setWarningMessage('');
+    }
+  };
+
+  return (
+    <div>
+      <input
+        id='nickname-input'
+        type='text'
+        placeholder='ex) todaysfail12'
+        value={nickname}
+        onChange={checkValidNickname}
+      />
+      {warningMessage && <div>{warningMessage}</div>}
+      <div>
+        <button disabled={!isValid}>시작하기</button>
+      </div>
+    </div>
+  );
+}
--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,5 +1,11 @@
 import React from 'react';
+import SignForm from '../../components/SignForm';
 
 export default function LoginPage() {
-  return <div>LoginPage</div>;
+  return (
+    <div>
+      <h2>닉네임을 정해주세요</h2>
+      <SignForm />
+    </div>
+  );
 }
