f5a581d12304bc6e1ef44a6b8425659ec52c8fd8
hayeonii <hayexn51@gmail.com>
2023-02-26 05:08:15 +0900

    [#3] design: 글로벌 스타일에 sr-only 속성 추가

--- src/style/GlobalStyle.jsx
+++ src/style/GlobalStyle.jsx
@@ -1,6 +1,15 @@
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
+  .sr-only {
+    position: absolute;
+    width: 1px;
+    height: 1px;
+    margin: -1px;
+    overflow: hidden;
+    clip-path: polygon(0 0, 0 0, 0, 0);
+  } 
+
   * {
     box-sizing: border-box;
   }
@@ -22,7 +31,7 @@ const GlobalStyle = createGlobalStyle`
     padding: 0;
   }
 
-  p{
+  h1,h2,h3, p, span{
     margin: 0;
   }
 `;
