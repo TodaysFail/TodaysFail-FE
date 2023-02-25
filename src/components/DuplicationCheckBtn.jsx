98b9cf29454a197a79061a3573539eb0bc402f5f
ssaid <94416699+swywssaid@users.noreply.github.com>
2023-02-25 20:33:44 +0900

    [#2] feat: 시작하기 버튼 비활성화 구현
    - 중복검사 완료 시, 시작하기 버튼 활성화

--- src/components/DuplicationCheckBtn.jsx
+++ src/components/DuplicationCheckBtn.jsx
@@ -1,14 +1,20 @@
 import axios from 'axios';
 import React from 'react';
 
-export default function DuplicationCheckBtn({ isValid, nickname, setWarningMessage }) {
+export default function DuplicationCheckBtn({ isValid, nickname, setWarningMessage, setIsDuplicated }) {
   const duplicationNicknameAPI = async (nickname) => {
     await axios
       .post('http://localhost:3000/login', {
         nickname: `${nickname}`,
       })
-      .then((res) => setWarningMessage(res, '중복된 닉네임입니다. 다시 시도하세요.'))
-      .catch((res) => setWarningMessage(res, '사용 가능한 닉네임입니다 : )'));
+      .then((res) => {
+        setWarningMessage(res, '사용 가능한 닉네임입니다 : )');
+        setIsDuplicated(false);
+      })
+      .catch((res) => {
+        setWarningMessage(res, '중복된 닉네임입니다. 다시 시도하세요.');
+        setIsDuplicated(true);
+      });
   };
 
   return (
--- src/components/SignForm.jsx
+++ src/components/SignForm.jsx
@@ -5,17 +5,21 @@ export default function SignForm() {
   const [nickname, setNickname] = useState('');
   const [isValid, setIsValid] = useState(false);
   const [warningMessage, setWarningMessage] = useState('');
+  const [isDuplicated, setIsDuplicated] = useState(true);
 
+  // 유효성 체크: 영문 소문자와 숫자만 사용 가능하고 최소 길이는 1 최대 길이는 10
   const checkValidNickname = (event) => {
     const input = event.target.value;
-    const regex = /^[a-z0-9]{1,10}$/; // 영문 소문자와 숫자만 사용 가능하고 최소 길이는 1 최대 길이는 12
+    const regex = /^[a-z0-9]{1,10}$/;
 
     if (regex.test(input)) {
       setNickname(input);
       setIsValid(true);
+      setIsDuplicated(true);
       setWarningMessage('유효한 닉네임입니다. 중복검사를 해주세요 : )');
     } else {
       setIsValid(false);
+      setIsDuplicated(true);
       setWarningMessage('영문 소문자와 숫자 조합으로 최대 12자까지 가능합니다');
     }
 
@@ -34,10 +38,15 @@ export default function SignForm() {
         value={nickname}
         onChange={checkValidNickname}
       />
-      <DuplicationCheckBtn isValid={isValid} nickname={nickname} setWarningMessage={setWarningMessage} />
+      <DuplicationCheckBtn
+        isValid={isValid}
+        nickname={nickname}
+        setWarningMessage={setWarningMessage}
+        setIsDuplicated={setIsDuplicated}
+      />
       {warningMessage && <div>{warningMessage}</div>}
       <div>
-        <button disabled={!isValid}>시작하기</button>
+        <button disabled={isValid ? isDuplicated : true}>시작하기</button>
       </div>
     </div>
   );
