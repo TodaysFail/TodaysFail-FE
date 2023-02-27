66a7352ea93fc8fbe7e0c49dd59266759576fd00
callmejeje <choije401@naver.com>
2023-02-27 16:25:37 +0900

    style: 로고 width props로 전달 가능하도록 함

--- src/components/common/Logo.jsx
+++ src/components/common/Logo.jsx
@@ -1,14 +1,14 @@
 import styled from 'styled-components';
 import TodaysFailLogo from '../../assets/logo.png';
 
-export default function Logo() {
-  return <MainLogo />;
+export default function Logo({ width }) {
+  return <MainLogo width={width} />;
 }
 
 const MainLogo = styled.img.attrs({
   src: TodaysFailLogo,
   alt: '오늘의 실패',
 })`
-  width: 150px;
+  width: ${(props) => props.width || '150px'};
   text-align: center;
 `;
