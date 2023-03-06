3b3f6f556fecf546179a28d7a658723239d46b99
hayeonii <hayexn51@gmail.com>
2023-03-06 15:48:21 +0900

    [#34] refactor: API 인스턴스 분리
    Co-authored-by: Jee-woo <jee-woo@users.noreply.github.com>
    Co-authored-by: ssaid <swywssaid@users.noreply.github.com>

---
+++ src/api/apiController.jsx
@@ -0,0 +1,24 @@
+import axios from 'axios';
+
+export const BASE_URL = process.env.REACT_APP_BASE_URL;
+
+// 사용자 정의 구성을 사용하는 axios 인스턴스 생성
+export default axios.create({
+  baseURL: BASE_URL,
+  headers: {
+    'Content-Type': 'application/json',
+    // Authorization: `Bearer`, 토큰
+  },
+});
+
+// 요청 인터셉터 추가
+axios.interceptors.request.use(
+  (config) => config,
+  (error) => Promise.reject(error),
+);
+
+// 응답 인터셉터 추가
+axios.interceptors.response.use(
+  (response) => response,
+  (error) => Promise.reject(error),
+);
