9f706928b3dbed56649756454a3fc2b3b2b4c2e6
hayeonii <hayexn51@gmail.com>
2023-03-20 17:23:25 +0900

    [#53] feat: 회원가입 페이지 추가

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -5,12 +5,14 @@ import LoginPage from '../pages/LoginPage/LoginPage';
 import MyPage from '../pages/MyPage/MyPage';
 import RecordPage from '../pages/RecordPage/RecordPage';
 import SharePage from '../pages/SharePage/SharePage';
+import SignUpPage from '../pages/SignUpPage/SignUpPage';
 
 export default function Router() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/login' element={<LoginPage />} />
+        <Route path='/signup' element={<SignUpPage />} />
         <Route path='/' element={<MainLayout />}>
           <Route index element={<MyPage />} />
           <Route path='/recordPage' element={<RecordPage />} />
