732f24d58e39866587520507aefcba0beef07e32
ssaid <94416699+swywssaid@users.noreply.github.com>
2023-03-22 18:24:41 +0900

    [#60] feat: 로그아웃 아이콘 생성

---
+++ src/components/common/Logout.jsx
@@ -0,0 +1,11 @@
+import { ReactComponent as LogoutBackground } from '../../assets/logout-background.svg';
+import { ReactComponent as LogoutIcon } from '../../assets/logout-icon.svg';
+
+export default function Logout() {
+  return (
+    <div>
+      <LogoutBackground />
+      <LogoutIcon />
+    </div>
+  );
+}
